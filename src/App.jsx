f8a2b2e2c97a87c5233725efd420d2b20d50f322
KashishMaurya <kashish.social.18@gmail.com>
2025-03-26 19:32:17 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { useState } from 'react'
 // import './App.css'
 import Header from './Components/Header'
 import Input from './Components/Input'
+import Footer from './Components/Footer'
 
 function App() {const [count, setCount] = useState(0)
 
   return (
     <>
       <Header />
-      <Input/>
+      <Input />
+      <Footer />
     </>
   )
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        position: "fixed",
+        bottom: 0,
+        width: "100%",
+        backgroundColor: "white",
+        color: "black",
+        textAlign: "center",
+        padding: "10px",
+      }}
+    >
+      Made with <b>&#x2661;</b> by KASHISH MAURYA
+    </footer>
+  );
+};
+
+export default Footer;
